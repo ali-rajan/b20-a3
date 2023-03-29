31d5bc0e8da2f8fddd1ed5bb7eb9d6b8f000390a
ali-rajan <ali.rajan@mail.utoronto.ca>
2023-03-29 16:18:33 -0400

    Added Flask boilerplate code

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
+
+@app.route("/add_word")
+def add_word():
+    return render_template("add-word.html")
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
 
 if __name__ == '__main__':
     app.run()
