ac194d763fa075e602893a38e385ee3b6bb34d10
ali-rajan <ali.rajan@mail.utoronto.ca>
2023-04-09 16:43:40 -0400

    Fixed typing sound bug and created separate file for leaderboard JS

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,5 @@
 const typingSound = document.getElementById("typing-sound");
-// Sliders for the leaderboard filters
-const scoreSlider = document.getElementById("score-slider");
-const guessSlider = document.getElementById("guesses-slider");
-const scoreFilterText = document.getElementById("score-text");
-const guessFilterText = document.getElementById("guess-text");
-
-document.addEventListener("keydown", typingSoundEffect);
-document.addEventListener("input", updateFilterTexts);
+document.addEventListener("input", typingSoundEffect);
 
 function typingSoundEffect()
 {
@@ -15,16 +8,4 @@ function typingSoundEffect()
      */
 
     typingSound.play();
-}
-
-function updateFilterTexts()
-{
-    /**
-     * Updates the text for the filters based on the new inputted value(s).
-     */
-
-    const minScore = scoreSlider.value;
-    const maxGuesses = guessSlider.value;
-    scoreFilterText.innerText = `Minimum score: ${minScore}`;
-    guessFilterText.innerText = `Maximum guesses: ${maxGuesses}`;
 }
